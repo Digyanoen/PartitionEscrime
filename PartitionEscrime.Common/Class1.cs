ff2d7a865a28fee8a2fe054e193aa74387cf746c
Digyanoen <julien.re.laurent@gmail.com>
2018-10-27 10:58:15 +0200

    Ajoutez des fichiers projet.

---
+++ PartitionEscrime.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PartitionEscrime.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ PartitionEscrime.Common/ViewModels/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartitionEscrime.Common.ViewModels
+{
+    public class Test
+    {
+        public String TextTest;
+
+        public Test()
+        {
+            TextTest = "Ceci est un test, rendez-vous !";
+        }
+    }
+}
